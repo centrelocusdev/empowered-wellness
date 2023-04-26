1565756090af1e2d2cb0b6c987b8a6c8d8633f6d
Rupali Haldiya <rupali7487@gmail.com>
2023-04-26 23:11:05 +0530

    adding animation

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,27 +3,39 @@ import { useNavigate } from "react-router-dom";
 import ButtonPrimary from "./ButtonPrimary";
 
 const Modal = () => {
-  const navigate = useNavigate()
-  const [close, setClose] = useState(false);
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+
   const handleCloseClick = () => {
-    setClose((close) => !close);
+    setIsOpen(false);
   };
 
   useEffect(() => {
-    setClose(false)
-  }, [])
+    setIsOpen(true);
+  }, []);
 
   return (
-    <div className={`${close && 'hidden'}
-    h-screen w-screen bg-[rgba(0,0,0,0.4)] fixed flex justify-center items-center`}>
+    <div
+      className={`fixed w-full h-full top-0 left-0 flex items-center justify-center transition-opacity duration-500 ease-in-out ${
+        isOpen ? "opacity-100 pointer-events-auto bg-[rgba(0,0,0,0.3)]" : "opacity-0 pointer-events-none"
+      }`}
+    >
       <div className="bg-white shadow-lg rounded-3xl md:w-3/5 md:m-auto m-5 md:p-16 p-5 text-center">
         <h2 className="md:text-3xl text-lg my-5">
           Get to know your level of Stress, Depression & Anxiety by taking a
           quick test
         </h2>
         <div className="flex flex-col items-center justify-center">
-          <ButtonPrimary text={"Test your mood"} handleClick={(e) => navigate('/wellness-measure')} />
-          <button onClick={handleCloseClick} className="text-gray-500 hover:underline">Close</button>
+          <ButtonPrimary
+            text={"Test your mood"}
+            handleClick={(e) => navigate("/wellness-measure")}
+          />
+          <button
+            onClick={handleCloseClick}
+            className="text-gray-500 hover:underline"
+          >
+            Close
+          </button>
         </div>
       </div>
     </div>
