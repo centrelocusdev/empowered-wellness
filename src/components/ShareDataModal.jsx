0efe474c1ae9bfec4bd41e1cb6334cbaecbee784
Rupali Haldiya <rupali7487@gmail.com>
2023-05-05 12:49:03 +0530

    fix styling bugs and add name field

--- src/components/ShareDataModal.jsx
+++ src/components/ShareDataModal.jsx
@@ -15,14 +15,20 @@ import {
 
 const ShareDataModal = ({ type, id, handleCloseClick, isOpen }) => {
   const navigate = useNavigate();
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
+  const [isDateSpan, setIsDateSpan] = useState(false);
 
   const handleEmailChange = (e) => {
-    setEmail(e.target.value)
-  }
+    setEmail(e.target.value);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
 
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
@@ -41,6 +47,7 @@ const ShareDataModal = ({ type, id, handleCloseClick, isOpen }) => {
     if (type == "mood test") {
       if (startDate && endDate) {
         res = await shareMoodTestDataSpan({
+          name,
           email,
           message,
           start_date: startDate,
@@ -48,6 +55,7 @@ const ShareDataModal = ({ type, id, handleCloseClick, isOpen }) => {
         });
       } else {
         res = await shareMoodTestData({
+          name,
           email,
           mood_id: id,
           message,
@@ -56,33 +64,28 @@ const ShareDataModal = ({ type, id, handleCloseClick, isOpen }) => {
     } else if (type == "journal") {
       if (startDate && endDate) {
         res = await shareJournalDataSpan({
-          journal_id: id,
-          user_id: doctorId,
+          name,
+          email,
           message,
           start_date: startDate,
           end_date: endDate,
         });
       } else {
         res = await shareJournalData({
+          name,
           email,
-          user_id: id,
+          journal_id: id,
           message,
         });
       }
     } else if (type == "assessment") {
       if (startDate && endDate) {
         res = await shareAssessmentDataSpan({
-          assessment_id: id,
-          user_id: doctorId,
+          name,
+          email,
           message,
           start_date: startDate,
           end_date: endDate,
-        });
-      } else {
-        res = await shareAssessmentData({
-          email,
-          user_id: id,
-          message,
         });
       }
     }
@@ -100,19 +103,29 @@ const ShareDataModal = ({ type, id, handleCloseClick, isOpen }) => {
           : "opacity-0 pointer-events-none"
       }`}
     >
-      <div className="bg-white shadow-lg rounded-3xl md:w-3/5 md:m-auto m-5 md:py-5  p-5 ">
+      <div className="bg-white md:h-[85%] overflow-y-scroll shadow-lg rounded-3xl md:w-3/5 md:m-auto m-5 md:py-5  p-5 ">
         <h2 className="md:text-3xl text-lg my-5 text-center">
           Share your data with Doctors
         </h2>
         <div className="flex flex-col items-center justify-center">
-        <div
+          <div
+            className="w-full flex justify-center "
+            onChange={handleNameChange}
+          >
+            <InputPrimary
+              name={"name"}
+              label={"name"}
+              placeholer={"Enter name of the receiver"}
+            />
+          </div>
+          <div
             className="w-full flex justify-center "
             onChange={handleEmailChange}
           >
             <InputPrimary
               name={"email"}
               label={"email"}
-              placeholer={"Enter receiver email address"}
+              placeholer={"Enter email of the receiver"}
             />
           </div>
           <div
@@ -127,22 +140,38 @@ const ShareDataModal = ({ type, id, handleCloseClick, isOpen }) => {
           </div>
 
           <div
-            className="w-full flex justify-center"
-            onChange={handleStartDateChange}
+            onChange={(e) => setIsDateSpan((prev) => !prev)}
+            className="flex gap-2 mt-4 md:w-1/2 accent-gray-800"
           >
-            <InputPrimary
-              type={"date"}
-              name={"start_date"}
-              label={"start date"}
-            />
+            <input type="checkbox" name="dateSpan" />
+            <label htmlFor="dateSpan">Share data with date span</label>
           </div>
 
-          <div
-            className="w-full flex justify-center"
-            onChange={handleEndDateChange}
-          >
-            <InputPrimary type={"date"} name={"end_date"} label={"end date"} />
-          </div>
+          {isDateSpan && (
+            <>
+              <div
+                className="w-full flex justify-center"
+                onChange={handleStartDateChange}
+              >
+                <InputPrimary
+                  type={"date"}
+                  name={"start_date"}
+                  label={"start date"}
+                />
+              </div>
+
+              <div
+                className="w-full flex justify-center"
+                onChange={handleEndDateChange}
+              >
+                <InputPrimary
+                  type={"date"}
+                  name={"end_date"}
+                  label={"end date"}
+                />
+              </div>
+            </>
+          )}
 
           <div className="flex justify-between md:w-1/2 w-full mt-4">
             <button
